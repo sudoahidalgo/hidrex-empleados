ebdb0cb09b8041564420ea41232c328856af1ecd
sudoahidalgo <ahidalgod@gmail.com>
2025-06-03 16:51:24 -0600

    Configure Tailwind build

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        hidrex: {
+          50: '#fef2f2',
+          100: '#fee2e2',
+          500: '#ef4444',
+          600: '#dc2626',
+          700: '#b91c1c',
+          800: '#991b1b',
+          900: '#7f1d1d'
+        }
+      }
+    }
+  },
+  plugins: [],
+};
