ef4835f7e367f3f598e787ed2f861aeb6a904433
sudoahidalgo <ahidalgod@gmail.com>
2025-06-03 16:37:38 -0600

    Read supabase config from env

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-// REEMPLAZA ESTOS VALORES CON LOS DE TU PROYECTO SUPABASE
-const supabaseUrl = 'https://ywawglicmuzluplnxyxa.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3YXdnbGljbXV6bHVwbG54eXhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgzNzA4NTYsImV4cCI6MjA2Mzk0Njg1Nn0.eaVhmm95dX-LijfG-tJ7UJ4gzFB5-95llXLtpTVs1c4'
+// Read Supabase credentials from environment variables
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
