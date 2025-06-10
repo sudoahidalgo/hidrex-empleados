f22c19024356306836358ac491517f49551e74d8
sudoahidalgo <ahidalgod@gmail.com>
2025-06-10 11:21:02 -0600

    Create env.js

---
+++ public/env.js
@@ -0,0 +1,4 @@
+window._env_ = {
+  SUPABASE_URL: "https://ywawglicmuzluplnxyxa.supabase.co",
+  SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3YXdnbGljbXV6bHVwbG54eXhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgzNzA4NTYsImV4cCI6MjA2Mzk0Njg1Nn0.eaVhmm95dX-LijfG-tJ7UJ4gzFB5-95llXLtpTVs1c4"
+};
